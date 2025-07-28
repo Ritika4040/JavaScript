5e0271602e5c67fa4c9b275636996e03bbab5b49
Ritika Dwivedi <ritikadwivedi40@gmail.com>
2025-07-28 19:29:29 +0530

    day-1

---
+++ variables-practice1.js
@@ -0,0 +1,36 @@
+console.log("hello js!");
+alert("welcome to js");
+console.log("let write js code");
+// variables in js are container to store some values
+// variables are containers for data.
+age=24;
+age+1;
+price=154.6;
+fullname="Tony stark";
+console.log(fullname);
+console.log(age);
+console.log(price);
+x=null;
+y=undefined;
+console.log(x);
+console.log(y);
+isFollow=true;
+console.log(isFollow);
+// JS is dynamically typed 
+/* VARIABLE RULES
+1. Variable names are case senstive;"a"&"A" is different.
+2. only letters,digits,underscore and $ is allowed.(NOT EVEN SPACE)
+3. only a letter,underscore or $ should be 1st character.
+4. Reserved words cannot be variable names.
+ 
+let,const & var
+var= variable can be re-declared & updated. A global scope variable.
+let= variable cannot be re-declared but can be updated.A block scope variable.
+const= variable cannot be re-declared or updated.A block scope variable.
+*/
+let name="stark";
+let totalprice=100;
+console.log(name);
+console.log(totalprice);
+
+const age=52;//cannot change it now
