d0c1967d0efb244c81751daa70f036b503615914
Ritika Dwivedi <ritikadwivedi40@gmail.com>
2025-08-07 16:13:02 +0530

    Day-3

---
+++ operators-practice3.js
@@ -0,0 +1,71 @@
+/*Arithmetic Operators
++: additional
+-: subtractional
+*: multiplicatonal
+/: divisional
+%: module
+**: Expontiation(power)
+
+unary operators are:
+increment: ++
+decrement: --
+a++ : post increment
+++a : pre increment
+b++ : post decrement
+++b : pre decrement
+ex: a+b here a+b are expression , + is operator and a,b are the operands.
+*/
+let a = 5;
+let b = 3;
+console.log("a+b = ",a+b);
+console.log("a-b = ",a-b);
+console.log("a*b = ",a*b);
+console.log("a/b = ",a/b);
+console.log("a%b = ",a%b);
+console.log("a**b = ",a**b);
+console.log("a = ",a,"b = ",b);
+a++; // first print then increment
+b--;
+console.log("a = ",a);
+console.log("b = ",b);
+console.log("++a = ",++a); // first increment then print
+
+/*
+Assignment Operator 
+= : equal to
++= : add equal to
+-= : sub equal to
+*= : multiply equal to
+%= : mod equal to
+*/
+ 
+a **= 4; 
+console.log("a = ",a);
+ 
+
+/*
+OUTPUT
+
+a+b =  8
+a-b =  2
+a*b =  15
+a/b =  1.6666666666666667
+a%b =  2
+a**b =  125
+a =  5 b =  3
+a =  6
+b =  2
+++a =  7
+a =  2401
+
+*/
+
+
+
+
+
+
+
+
+
+
